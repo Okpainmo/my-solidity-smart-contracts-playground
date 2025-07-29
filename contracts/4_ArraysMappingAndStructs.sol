6e02ad39ae995d16d5c5ffb245e15d8e148e686a
Okpainmo <okpainmoandrew@gmail.com>
2025-07-29 15:19:20 +0100

    chore: renamed contracts to show order and reflect complexity levels + more

--- contracts/4_ArraysMappingAndStructs.sol
+++ contracts/4_ArraysMappingAndStructs.sol
@@ -3,27 +3,27 @@ pragma solidity >=0.8.2 <0.9.0;
 
 /**
  * @title Cashbook for Monthly Payment Tracking
- * @author 
+ * @author
  * @notice This contract allows an accountancy firm to register users, track wallet balances, and record monthly payouts.
  * @dev Demonstrates the use of structs, arrays, mappings and data access control.
  */
 contract ArraysMappingsAndStructs {
     /// @notice Represents a payout made by a user
     struct Payout {
-        address paidBy;        ///< Address of the payer
-        uint256 amountPaid;    ///< Amount paid
-        uint256 paymentDate;   ///< Timestamp of payment
-        address recipient;     ///< Recipient of the payout
-        uint256 id;            ///< Unique identifier for payout
+        address paidBy; ///< Address of the payer
+        uint256 amountPaid; ///< Amount paid
+        uint256 paymentDate; ///< Timestamp of payment
+        address recipient; ///< Recipient of the payout
+        uint256 id; ///< Unique identifier for payout
     }
 
     /// @notice Represents a registered user
     struct User {
-        string name;               ///< Full name of the user
-        uint256 walletBalance;     ///< User's current wallet balance
+        string name; ///< Full name of the user
+        uint256 walletBalance; ///< User's current wallet balance
         address userWalletAddress; ///< User's Ethereum wallet address
-        Payout[] payouts;          ///< List of all payouts made by the user
-        uint256 id;                ///< Unique identifier for the user
+        Payout[] payouts; ///< List of all payouts made by the user
+        uint256 id; ///< Unique identifier for the user
     }
 
     User[] internal users;
@@ -31,7 +31,8 @@ contract ArraysMappingsAndStructs {
 
     mapping(address => User) internal userData_ByAddress;
     mapping(uint256 => User) internal userData_ById;
-    mapping(address => Payout[]) internal payoutMapping;
+    mapping(address => Payout[]) internal payoutMapping_AdminHas;
+    mapping(address => Payout[]) internal payoutMapping_UserPayout;
     mapping(uint256 => Payout) internal payoutData;
     mapping(address => bool) public isRegistered_ByAddress;
     mapping(uint256 => bool) public isRegistered_ById;
@@ -44,11 +45,18 @@ contract ArraysMappingsAndStructs {
      * @param _userWalletAddress The Ethereum address of the user
      * @return userId The unique ID assigned to the registered user
      */
-    function registerUser(string calldata _name, uint256 _walletBalance, address _userWalletAddress) public returns(uint256) {
-        require(!isRegistered_ByAddress[_userWalletAddress], "A user already exist with this wallet address. Duplicate users not allowed");
+    function registerUser(
+        string calldata _name,
+        uint256 _walletBalance,
+        address _userWalletAddress
+    ) public returns (uint256) {
+        require(
+            !isRegistered_ByAddress[_userWalletAddress],
+            "A user already exist with this wallet address. Duplicate users not allowed"
+        );
 
         Payout[] memory userPayouts;
-        users.push(); 
+        users.push();
 
         User memory userToUpdate = users[users.length - 1];
 
@@ -87,8 +95,11 @@ contract ArraysMappingsAndStructs {
         address _userWalletAddress,
         Payout[] memory _payouts,
         uint256 _id
-    ) public returns(User memory) {
-        require(isRegistered_ById[_id], "A registered user with this id not found or does not exist");
+    ) public returns (User memory) {
+        require(
+            isRegistered_ById[_id],
+            "A registered user with this id not found or does not exist"
+        );
 
         User memory user = userData_ById[_id];
 
@@ -119,7 +130,7 @@ contract ArraysMappingsAndStructs {
         uint256 paymentDate,
         address recipient,
         uint256 id
-    ) public view returns(Payout memory) {
+    ) public view returns (Payout memory) {
         Payout memory payout = payoutData[id];
 
         payout.paidBy = paidBy;
@@ -136,8 +147,13 @@ contract ArraysMappingsAndStructs {
      * @param _userWalletAddress The wallet address of the user
      * @return user The full User struct
      */
-    function getUserAccountData(address _userWalletAddress) public view returns(User memory) {
-        require(isRegistered_ByAddress[_userWalletAddress], "A user with this wallet address not found or does not exist");
+    function getUserAccountData(
+        address _userWalletAddress
+    ) public view returns (User memory) {
+        require(
+            isRegistered_ByAddress[_userWalletAddress],
+            "A user with this wallet address not found or does not exist"
+        );
         return userData_ByAddress[_userWalletAddress];
     }
 
@@ -153,7 +169,7 @@ contract ArraysMappingsAndStructs {
         address _recipient
     ) public {
         payouts.push();
-        Payout storage payoutToUpdate = payouts[payouts.length -  1];
+        Payout storage payoutToUpdate = payouts[payouts.length - 1];
 
         payoutToUpdate.paidBy = _paidBy;
         payoutToUpdate.amountPaid = _amountPaid;
@@ -161,18 +177,22 @@ contract ArraysMappingsAndStructs {
         payoutToUpdate.recipient = _recipient;
         payoutToUpdate.id = payouts.length + 1;
 
-        Payout[] storage adminPayoutsToUpdate = payoutMapping[_paidBy];
+        Payout[] storage adminPayoutsToUpdate = payoutMapping_AdminHas[_paidBy];
         adminPayoutsToUpdate.push(payoutToUpdate);
 
         adminHasRecordedPayouts[_paidBy] = true;
         payoutData[payouts.length + 1] = payoutToUpdate;
+
+        User storage user = userData_ByAddress[_recipient];
+        Payout[] storage userPayouts = user.payouts;
+        userPayouts.push(payoutToUpdate);
     }
 
     /**
      * @notice Returns all payouts made on the platform
      * @return payoutsArray The list of all payouts
      */
-    function getAllPlatformPayouts() public view returns(Payout[] memory) {
+    function getAllPlatformPayouts() public view returns (Payout[] memory) {
         return payouts;
     }
 
@@ -180,7 +200,7 @@ contract ArraysMappingsAndStructs {
      * @notice Returns all registered users on the platform
      * @return usersArray The list of all users
      */
-    function getAllPlatformUsers() public view returns(User[] memory) {
+    function getAllPlatformUsers() public view returns (User[] memory) {
         return users;
     }
 
@@ -189,10 +209,31 @@ contract ArraysMappingsAndStructs {
      * @param _userWalletAddress The user's wallet address
      * @return userPayouts The payouts made by the user
      */
-    function getAllUserPayouts(address _userWalletAddress) public view returns(Payout[] memory) {
-        require(isRegistered_ByAddress[_userWalletAddress], "A user with this wallet address not found or does not exist");
+    function getAllUserPayouts(
+        address _userWalletAddress
+    ) public view returns (Payout[] memory) {
+        require(
+            isRegistered_ByAddress[_userWalletAddress],
+            "A user with this wallet address not found or does not exist"
+        );
 
         User memory user = userData_ByAddress[_userWalletAddress];
         return user.payouts;
+    }
+
+    /**
+     * @notice Retrieves all payouts made by a given admin address
+     * @param _adminAddress The Ethereum address of the admin
+     * @return payoutsList List of payouts recorded by this admin
+     */
+    function getAllPayoutsMadeByAnAdmin(
+        address _adminAddress
+    ) public view returns (Payout[] memory) {
+        require(
+            adminHasRecordedPayouts[_adminAddress],
+            "No payouts made by this admin"
+        );
+
+        return payoutMapping_AdminHas[_adminAddress];
     }
 }
